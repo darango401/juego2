ee6d6c80b52aa96beb95a55bedaa9a9d5e5bbabc
Daniel Altman <44586460+darango401@users.noreply.github.com>
2021-05-11 18:06:41 -0500

    Menu y Popup niveles

--- app/src/main/java/com/example/sudoku_app/MenuActivity.java
+++ app/src/main/java/com/example/sudoku_app/MenuActivity.java
@@ -3,13 +3,25 @@ package com.example.sudoku_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
 public class MenuActivity extends Activity {
-
+    Button buttonJugar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        buttonJugar = (Button)findViewById(R.id.buttonJugar);
+        buttonJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent(getApplicationContext(), MenuPopup.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/sudoku_app/MenuPopup.java
@@ -0,0 +1,32 @@
+package com.example.sudoku_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
+
+public class MenuPopup extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_popup);
+        getActionBar().hide();
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int)(width *.8), (int)(height*.5));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -20;
+
+        getWindow().setAttributes(params);
+    }
+}
